81a3ac399ddb90b0ee9f4739a44042a074808d24
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2023-05-27 14:57:51 +0530

    Add files via upload

---
+++ Basic/ADDITION.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int Num1=0,Num2=0,sum=0;
+	clrscr();
+	printf("Enter two numbers");
+	scanf("%d%d",&Num1,&Num2);
+	sum=Num1+Num2;
+	printf("\n Addition of two numbers is %d",sum);
+	getch();
+}
