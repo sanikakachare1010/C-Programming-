6ee9bb4700ec73f6eed3be2ab0bd7061dd65e257
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2023-05-27 15:21:35 +0530

    Add files via upload

---
+++ Basic/DIVISION.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int Num1,Num2,Div;
+	clrscr();
+	printf("Enter any two numbers:");
+	scanf("%d%d",&Num1,&Num2);
+	Div=Num1/Num2;
+	printf("Division of two number is%d",Div);
+	getch();
+}
