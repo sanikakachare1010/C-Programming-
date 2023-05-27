dc4f282e2db559daaf8fe370bfeb0ed781cf895f
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2023-05-27 14:47:55 +0530

    Add files via upload

---
+++ Basic/STUDENT_.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	long int Mob;
+	char Name,Add,Class;
+	clrscr();
+	printf("Enter your Name:");
+	scanf("%s",&Name);
+	printf("Enter your Address:");
+	scanf("%s",&Add);
+	printf("Enter your Mobile number:");
+	scanf("%ld",&Mob);
+	printf("Enter your Class:");
+	scanf("%s",&Class);
+	getch();
+}
