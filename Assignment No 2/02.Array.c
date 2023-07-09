40d904845cfc9aee33fb5fa7905b669a6be0779a
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2023-07-09 23:34:30 +0530

    Add files via upload

---
+++ Assignment No 2/02.Array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int array[4] = {1,2,3,4};
+
+    printf("\n value of array element : %d",array[1]);
+    printf("\n value of array element : %d",array[0]);
+    printf("\n value of array element : %d",array[2]);
+    printf("\n value of array element : %d",array[3]);
+
+    getch();
+    return 0;
+
+}
