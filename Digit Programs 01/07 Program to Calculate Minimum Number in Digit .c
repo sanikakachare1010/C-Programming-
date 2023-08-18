ca1bd6768e0303a1f9145d6c7e0d601a94596a17
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2023-08-18 22:55:56 +0530

    Add files via upload

---
+++ Digit Programs 01/07 Program to Calculate Minimum Number in Digit .c
@@ -0,0 +1,32 @@
+/// Program to Calculate Minimum Number in Digit
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No = 0, Temp = 0, Dig = 0, Min = 0;
+
+    printf("\n Enter Positive Number : ");
+    scanf("%d",&No);
+
+    Temp = No;
+
+    Min = Temp %10;
+
+    while(Temp > 0)
+    {
+        Dig = Temp % 10;
+
+        if(Dig < Min)
+        {
+            Min =Dig;
+        }
+
+        Temp = Temp /10;
+    }
+
+    printf("\n Minimum  Number in %d is : %d",No,Min);
+
+    getch();
+    return 0;
+
+}
