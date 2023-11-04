e68832e5d1d0bef5bb2ddc362376f3969545051f
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2023-11-04 23:43:38 +0530

    Update 01.Print_Characters_Using_Array.c

--- Array/01.Print_Characters_Using_Array.c
+++ Array/01.Print_Characters_Using_Array.c
@@ -2,7 +2,7 @@
 #include<conio.h>
 int main()
 {
-    char array[4] = {'F','O','R','K'}
+    char array[4] = {'F','O','R','K'};
 
     printf("\n Value of element 1 : %c.",array[0]);
     printf("\n Value of element 2 : %c.",array[1]);
